a930f74a5a58c9bce55eb4485e97f2b68df036de
Leonardo Trento <leotrento88@gmail.com>
2025-06-10 23:19:15 -0300

    backend finalizado

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from db import db
 from models import Atividade
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados.db'
 db.init_app(app)
+
+@app.route('/')
+def home():
+    registros = db.session.query(Atividade).all()
+    return render_template('home.html', registros=registros)
+
+@app.route('/cadastrar', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'GET':
+        return render_template('cadastrar_tarefa.html')
+    elif request.method == 'POST':
+        nome = request.form['nomeForm']
+        tarefa = request.form['tarefaForm']
+        
+        nova_tarefa = Atividade(nome=nome, tarefa=tarefa)
+        db.session.add(nova_tarefa)
+        db.session.commit()
+        return redirect(url_for('home'))
+    
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    registro = db.session.query(Atividade).filter_by(id=id).first()
+    db.session.delete(registro)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class Atividade(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(50), nullable=False)
-    tarefa = db.Column(db.String(10), nullable=False)
+    tarefa = db.Column(db.String(100), nullable=False)
     
     def __repr__(self):
-        return f'<{self.nome}>'
+        return f'<{self.nome} : {self.tarefa}>'
